aafbfd2c3399a1180463157349c336c3533ad120
mercytv932 <Mercytv932@gmail.com>
2026-04-03 22:25:31 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator">
+      <div className="display">0</div>
+      <div className="buttons">
+      <button>1</button> <button>2</button> <button>3</button> <button>4</button> <button>5</button>
+      <button>6</button> <button>7</button> <button>8</button> <button>9</button> <button>0</button>
+
+      <button>+</button> <button>-</button> <button>x</button> <button>÷</button> <button>=</button> 
+      <button>C</button>
+    </div>
+
     </div>
   );
 }
