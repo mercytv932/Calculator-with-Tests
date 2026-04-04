484631f06ae57a3d9df1d1534fb57f0e2057aecf
mercytv932 <Mercytv932@gmail.com>
2026-04-03 21:48:47 -0400

    Add tests for multiply and divide functions

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-import {add, subtract} from './calculator';
+import {add, subtract, multiply, divide} from './calculator';
 
 test('add two numbers', () =>{
   expect(add(2,3)).toBe (5);
@@ -6,5 +6,18 @@ test('add two numbers', () =>{
 
 
 test('subtract two numbers', () =>{
-  expect(subtract(2,3)).toBe (-1);
+  expect(subtract(3,2)).toBe (1);
 });
+
+test('multiply two numbers', () => {
+  expect(multiply(2,3)).toBe (6)
+})
+
+
+test('divide two numbers', () => {
+  expect(divide(6,3)).toBe (2)
+})
+
+test('divide by zero', () =>{
+  expect(divide(6,0)).toBe(Infinity);
+})
