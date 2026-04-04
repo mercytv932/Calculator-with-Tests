ab283ed8ae8fd81553e8f04fb05b69f49fd2f2ed
mercytv932 <Mercytv932@gmail.com>
2026-04-03 21:47:56 -0400

    Add multiply and divide functions to calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 function add(a, b){
- return a+b;
+ return a + b;
 }
 
 function subtract(a, b){
-  return a-b;
+  return a - b;
 }
 
-export {add, subtract};
+function multiply(a,b){
+  return a * b;
+}
+
+
+function divide(a,b){
+  return a / b;
+}
+
+export {add, subtract, multiply, divide};
+
