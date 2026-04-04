35842a8bd2a4536f874b34ce3117970d2fc03275
mercytv932 <Mercytv932@gmail.com>
2026-04-03 20:32:48 -0400

    Add subtract test and import both functions

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,10 @@
-import add from './calculator';
+import {add, subtract} from './calculator';
 
 test('add two numbers', () =>{
   expect(add(2,3)).toBe (5);
+});
+
+
+test('subtract two numbers', () =>{
+  expect(subtract(2,3)).toBe (-1);
 });
