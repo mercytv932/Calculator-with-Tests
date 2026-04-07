86c0854303cdddf987e4f2cec3ed0fe15f2dd8c7
mercytv932 <Mercytv932@gmail.com>
2026-04-07 02:25:54 -0400

    Add test for calculators display rendering

--- App.test.js
+++ App.test.js
@@ -1,2 +1,7 @@
-test('renders calculators', () => {
+import {render, screen, fireEvent} from '@testing-library/react';
+import App from './App';
+
+test('renders calculators display', () => {
+  render(<App/>);
+  expect(screen.getByText('0')).toBeInTheDocument();
 });
