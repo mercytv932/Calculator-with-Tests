b49fc7fc6fca900280f66f1e8e45e92b58bd0f6f
mercytv932 <Mercytv932@gmail.com>
2026-04-08 00:35:29 -0400

    Refactor calculator button components for clarity

--- App.js
+++ App.js
@@ -7,21 +7,22 @@ function App() {
   
   return (
     <div className="Calculator">
+      <h1 className='h1'>Calculator</h1>
       <div className="display">{display}</div>
       <div className="buttons">
-      <button onClick={() => setDisplay (display === '0' ? '1' : display + '1')}>1</button> <button onClick={() => setDisplay(display === '0' ? '2' : display + '2')}>2</button>
+      < button onClick={() => setDisplay (display === '0' ? '1' : display + '1')}>1</button> <button onClick={() => setDisplay(display === '0' ? '2' : display + '2')}>2</button>
        <button onClick={() => setDisplay(display === '0' ? '3' : display + '3')}>3</button> <button onClick={() => setDisplay(display === '0' ? '4' : display + '4')}>4</button> 
        <button onClick={() => setDisplay(display === '0' ? '5' : display + '5')}>5</button>
       <button onClick={() => setDisplay(display === '0' ? '6' : display + '6')}>6</button> <button onClick={() => setDisplay(display === '0' ? '7' : display + '7')}>7</button>
        <button onClick={() => setDisplay(display === '0' ? '8' : display + '8')}>8</button> <button onClick={() => setDisplay(display === '0' ? '9' : display + '9')}>9</button>
         <button onClick={() => setDisplay(display === '0' ? '0' : display + '0')}>0</button>
 
-      <button onClick={() => setDisplay(display + '+')}>+</button> <button onClick={() => setDisplay(display + '-')}>-</button>
-       <button onClick={() => setDisplay(display + '*')}>x</button> <button onClick={()=> setDisplay(display +'/')}>÷</button>
-        <button onClick={() => setDisplay(String(eval(display)))}>=</button> <button onClick={()=> setDisplay('0')}>C</button>
-        <button onClick={() => setDisplay(display.length > 1 ? display.slice(0, -1) : '0')}>Del</button>
-    </div> 
-
+      <button className='operatorBtn' onClick={() => setDisplay(display + '+')}>+</button> <button className='operatorBtn' onClick={() => setDisplay(display + '-')}>-</button>
+       <button className='operatorBtn' onClick={() => setDisplay(display + '*')}>x</button> <button className='operatorBtn' onClick={()=> setDisplay(display +'/')}>÷</button>
+        <button className='equalBtn' onClick={() => setDisplay(String(eval(display)))}>=</button> <button className='CBtn' onClick={()=> setDisplay('0')}>C</button>
+        <button className='delBtn' onClick={() => setDisplay(display.length > 1 ? display.slice(0, -1) : '0')}>Del</button>
+    </div>
+    
     </div>
   );
 }
